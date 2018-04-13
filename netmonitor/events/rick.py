dc553ae71f0442483326d4db958668a6d08f42be
lucyoa <marcin@threat9.com>
2018-04-13 15:49:03 +0200

    Adding event Rick

---
+++ netmonitor/events/rick.py
@@ -0,0 +1,23 @@
+import sys
+from monitor.event import Event
+
+
+class Event(Event):
+    def __init__(self):
+        self.opened = False
+
+    def trigger(self, event, asset):
+        if not self.opened and event is "appeared":
+            self.opened = True
+            return True
+
+        return False
+    
+    def execute(self):
+        if sys.platform == "darwin":
+            client = "open"
+        else:
+            client = "sensible-browser"
+
+        cmd = "{} https://www.youtube.com/watch?v=dQw4w9WgXcQ".format(client)
+        self.exec(cmd)
